a08f86b8770e0c3a4faf5911189e3e887af482e6
Yamkela Zungula <zungulay@gmail.com>
2023-02-18 23:19:47 +0200

    updated axios base url

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import { createApp } from "vue";
+import axios from "axios";
 import { createPinia } from "pinia";
+import { createApp } from "vue";
 
 import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
+
+axios.defaults.baseURL = "http://localhost:3000";
 
 const app = createApp(App);
 
