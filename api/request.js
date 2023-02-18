1427b19217038202f3f91d9c733975a9028c7bab
Yamkela Zungula <zungulay@gmail.com>
2023-02-18 20:37:18 +0200

    Added request wrapper method

---
+++ api/request.js
@@ -0,0 +1,63 @@
+import axios from "axios";
+
+/**
+ * Runs a request a to the back-end using the given request data.
+ * @param {Object} request request data
+ */
+export function request(request) {
+  axios.defaults.baseURL = "http://localhost:3000";
+
+  if (request && request.method) {
+    switch (request.method.toLowerCase()) {
+      case "get":
+        return getRequest(request);
+      case "post":
+        return postRequest(request);
+      case "put":
+        return updateRequest(request);
+      case "delete":
+        return deleteRequest(request);
+      default:
+        break;
+    }
+  } else {
+    console.error(`ERROR: Incorrect request.`);
+  }
+}
+
+function getRequest(request) {
+  if (request && request.params) {
+    return axios.get(request.url, { params: request.params });
+  }
+  return axios.get(request.url);
+}
+
+function postRequest(request) {
+  if (request && request.data) {
+    return axios.post(request.url, request.data);
+  } else {
+    console.error(
+      `ERROR: Incorrect on ${request.method} request: ${request.url}`
+    );
+  }
+}
+
+function updateRequest(request) {
+  if (request && request.method && request.data) {
+    return axios.put(request.url, request.data);
+  } else {
+    console.error(
+      `ERROR: Incorrect on ${request.method} request: ${request.url}`
+    );
+  }
+}
+
+function deleteRequest(request) {
+  if (request && request.data) {
+    return axios.delete(request.url, { params: request.params });
+  } else {
+    console.error(
+      `ERROR: Incorrect on ${request.method} request: ${request.url}`
+    );
+  }
+}
