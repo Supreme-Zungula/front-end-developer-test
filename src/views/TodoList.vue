b056b88bbcff881bd3f8d96c986302c345fd5f6b
Yamkela Zungula <zungulay@gmail.com>
2023-02-19 14:26:34 +0200

    fetch todos from server

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -5,6 +5,7 @@
   </div>
 </template>
 <script>
+import { getTodoList } from "@/api/todoList";
 import AddItem from "./AddItem.vue";
 import TodoItem from "./TodoItem.vue";
 
@@ -14,11 +15,24 @@ export default {
     AddItem,
     TodoItem,
   },
+  data() {
+    return {
+      todoList: [],
+    };
+  },
   mounted() {
-    
+    this.fetchTodoList();
   },
   methods: {
-
+    fetchTodoList() {
+      getTodoList()
+        .then((res) => {
+          this.todoList = res.data.data;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
   },
 };
 </script>
