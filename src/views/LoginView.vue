f05aac3b3b27063f15ba5f53d3106cecc7e86a47
Yamkela Zungula <zungulay@gmail.com>
2023-02-19 07:11:42 +0200

    code clean up

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,13 +1,14 @@
 <template>
-  <div>
+  <div class="login-form">
     <h1>Login</h1>
   </div>
 </template>
 <script>
 export default {
-  
-}
+  name: "LoginView",
+  data() {
+    return {};
+  },
+};
 </script>
-<style lang="">
-  
-</style>
+<style></style>
