0c3f8e11a16f2770ceaba7f0cb940887ae009668
Yamkela Zungula <zungulay@gmail.com>
2023-02-19 11:12:03 +0200

    added basic styling

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="login-form">
+  <div class="container">
     <h1>Login</h1>
-    <form>
+    <form class="login-form">
       <label>Username:</label>
       <input type="text" name="username" v-model="username" />
       <label for="password">Password:</label>
       <input type="password" name="password" v-bind="password" />
-      <button @click="handleLogin">Login</button>
     </form>
+    <div class="btn-container">
+      <button @click="handleLogin">Login</button>
+    </div>
     <RouterLink to="/register">More options...</RouterLink>
   </div>
 </template>
@@ -21,15 +23,20 @@ export default {
     };
   },
   methods: {
-    handleLogin() {
-      localStorage.a;
-    },
+    handleLogin() {},
   },
 };
 </script>
 <style scoped>
+.container h1 {
+  display: inline;
+  font-size: 25px;
+}
 .login-form {
   display: block;
+  border: 1px solid white;
+  border-radius: 15px;
+  padding: 15px;
   max-width: 600px;
   margin: 20px auto;
 }
@@ -39,15 +46,20 @@ export default {
 }
 
 .login-form input {
-  border: 2px grey;
+  border: 2px white;
+  background: grey;
   border-radius: 5px;
   width: 100%;
 }
 
-.login-form button {
+.btn-container {
+  place-items: center;
+}
+.btn-container button {
+  min-width: 100px;
   background-color: aqua;
   color: black;
-  margin: 5px auto;
+  margin: 15px auto;
   padding: 5px;
   border-radius: 5px;
   border: 1px solid grey;
