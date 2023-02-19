9f33c97d1a78f2e5a62f8f681ccaa01d43357b46
Yamkela Zungula <zungulay@gmail.com>
2023-02-19 15:53:27 +0200

    Conditionally disable add task
    Disable add task button if not all info is provided

--- src/views/AddItem.vue
+++ src/views/AddItem.vue
@@ -6,27 +6,52 @@
         type="text"
         name="title"
         placeholder="Task  title"
-        class="min-w-full text-lg p-1"
+        class="min-w-full text-lg p-1 text-black"
+        v-model="task.title"
       />
       <input
         type="text"
         name="detail"
         placeholder="Task detail"
-        class="min-w-full text-lg p-1"
+        class="min-w-full text-lg p-1 text-black"
+        v-model="task.detail"
       />
     </div>
     <div class="btn-container grid grid-cols-2 gap-x-4 gap-y-2 my-2">
-      <button class="bg-green-500 max-w-full min-h-[50px]">Add Task</button>
+      <button
+        class="bg-green-500 max-w-full min-h-[50px]"
+        :disabled="!detailsFilled()"
+      >
+        Add Task
+      </button>
       <button class="min-h-[50px] bg-cyan-500">Select All</button>
       <button class="min-h-[50px] bg-red-500">Delete All</button>
     </div>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  name: "AddItem",
+  data() {
+    return {
+      task: {
+        title: null,
+        detail: null,
+      },
+    };
+  },
+  methods: {
+    detailsFilled() {
+      if (this.task.title && this.task.detail) {
+        return true;
+      }
+      return false;
+    },
+  },
+};
 </script>
 <style>
-.add-todo-form input {
-  margin-top: 15px;
+.btn-container button:disabled {
+  opacity: 0.5;
 }
 </style>
