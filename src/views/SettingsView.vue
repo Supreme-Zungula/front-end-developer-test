a3bf153d342626dacc2489ba8f1b56856439e2a1
Yamkela Zungula <zungulay@gmail.com>
2023-02-20 23:51:35 +0200

    Created SetttingsView.vue

---
+++ src/views/SettingsView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-white">
+    <button class="bg-cyan-500 text-white">Back</button>
+    <div>
+      <h2 class="text-xl font-bol">Settings</h2>
+      <ul>
+        <li>Review account details</li>
+        <li>Delete my account</li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "SettingsView",
+};
+</script>
+<style></style>
