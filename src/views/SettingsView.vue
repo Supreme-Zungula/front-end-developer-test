f8e227f90617b101b9f5382eff64e85095c6babe
Yamkela Zungula <zungulay@gmail.com>
2023-02-21 10:59:50 +0200

    Added go back logic

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="bg-white">
-    <button class="bg-cyan-500 text-white">Back</button>
+    <button
+      class="bg-cyan-500 text-white p-2 rounded-full w-20"
+      @click="goToPreviousPage"
+    >
+      {{ "< Back" }}
+    </button>
     <div>
       <h2 class="text-xl font-bol">Settings</h2>
       <ul>
@@ -13,6 +18,11 @@
 <script>
 export default {
   name: "SettingsView",
+  methods: {
+    goToPreviousPage() {
+      this.$router.go(-1);
+    },
+  },
 };
 </script>
 <style></style>
