f8574023ce6ac0acb6525b902b3e54556c365119
Yamkela Zungula <zungulay@gmail.com>
2023-02-22 02:30:18 +0200

    Created ReviewAccountDetails component

---
+++ src/views/ReviewAccountDetails.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-white text-black p-4">
+    <h1 class="text-xl font-bold">Review Account Details</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "ReviewAccount",
+};
+</script>
+<style></style>
